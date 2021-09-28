4e866950573c86a785c7c74645a739552ad39c83
Bennie the Awesome <bennie@comparisure.co.za>
2021-09-28 12:12:05 +0200

    requirements

--- genbot/data.py
+++ genbot/data.py
@@ -1,3 +1,12 @@
-class Dataset:
-    def __init__(self):
-        print('initializing')
+import pandas as pd
+from torch.utils.data import Dataset as TorchDataset
+
+
+class Dataset(TorchDataset):
+    def __init__(self, df: pd.DataFrame):
+        pass
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../sample.csv')
+    dataset = Dataset(df)
