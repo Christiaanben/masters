4def2a6a764b123d7d33ed23303b83530fa91c9a
Bennie <bennie@comparisure.co.za>
2023-06-06 00:35:05 +0200

    try f1 optimization

--- genbot/__main__.py
+++ genbot/__main__.py
@@ -4,6 +4,7 @@ import logging
 import torch
 from torch.optim.adam import Adam
 from torch.utils.data import DataLoader
+from torchmetrics.functional.classification import multilabel_f1_score
 from tqdm import tqdm
 from transformers import DistilBertTokenizerFast
 
@@ -14,6 +15,7 @@ from models import IntentClassifier, IntentPredictor
 DATASET_FILENAME = '../data/clean/customer_support_twitter_full.json'
 TESTSET_FILENAME = '../data/clean/customer_support_twitter_full_test.json'
 N_EPOCHS = 2
+RETRAIN = True
 DEVICE = 'cuda'
 
 logging.basicConfig(level=logging.INFO, format='[{levelname}] {message}', style='{')
@@ -36,7 +38,7 @@ def train_intent_classifier(classifier: IntentClassifier, dataset: IntentClassif
     for epoch in tqdm(range(n_epochs)):
         logging.info(f"Epoch {epoch}")
         running_loss = 0.
-        for batch in DataLoader(dataset, batch_size=2, shuffle=True):
+        for batch in DataLoader(dataset, batch_size=6, shuffle=True):
             torch.cuda.empty_cache()
             batch = {k: v.to(DEVICE) for k, v in batch.items()}
             outputs = classifier(batch)
@@ -68,9 +70,16 @@ def evaluate_intent_classifier(classifier, dataset: IntentClassificationDataset)
         loss = outputs.loss
         losses.append(loss.item())
     avg_loss = sum(losses) / len(losses)
-    logging.info(f'Validation loss: {avg_loss:.5f}')
-    f1_metrics = MultilabelF1Score(num_labels=targets.shape[1], threshold=0.5)
-    logging.info(f'F1 metrics: {f1_metrics(predictions, targets)}')
+    import numpy as np
+    avg_prob = float(np.average(torch.sigmoid(predictions).detach().numpy()))
+    print('predictions:', predictions)
+    print('targets:', targets)
+    print('sigmoid predictions:', torch.sigmoid(predictions))
+    print('avg prob:', avg_prob)
+    f1_score = multilabel_f1_score(predictions, targets, num_labels=targets.shape[1], threshold=avg_prob, average='micro')
+    f1_score_low_threshold = multilabel_f1_score(torch.sigmoid(predictions), targets, num_labels=targets.shape[1], threshold=0.1, average='micro')
+    f1_score_optimal_threshold = multilabel_f1_score(torch.sigmoid(predictions), targets, num_labels=targets.shape[1], threshold=float(f1_score/2), average='micro')
+    logging.info(f'Validation Loss: {avg_loss:.5f}; F1 score: {f1_score:.5f}, F1 score (low threshold): {f1_score_low_threshold:.5f}, F1 score (optimal threshold): {f1_score_optimal_threshold:.5f}')
     return avg_loss
 
 
@@ -162,7 +171,11 @@ def main():
     classifier_testset = get_classifier_testset(classifier_dataset.intents, classifier_tokenizer)
     # Setup, train, & evaluate classifier
     classifier = init_classifier(classifier_dataset)
-    train_intent_classifier(classifier, classifier_dataset, validation_dataset=classifier_testset, n_epochs=3)  # 30 epochs
+    train_intent_classifier(
+        classifier,
+        classifier_dataset,
+        validation_dataset=classifier_testset,
+        n_epochs=30)  # 30 epochs
     loss = evaluate_intent_classifier(classifier, classifier_testset)
     logging.info(f'Evaluation loss: {loss}')
     # Eval: classifier_dataset.intents[torch.argmax(classifier.forward(classifier_tokenizer(text, return_tensors='pt')))]
